9d26fecca3da9d8c4842a683ef76580879b35c29
kowshik gunda <kgunda@uco.edu>
2020-02-13 23:49:01 -0600

    creating the way to create files and append data to it and also deleting it if nessarry

--- scrp.py
+++ scrp.py
@@ -7,3 +7,29 @@ def prj_dir(directory):
         print('creating a dir' + directory)
         os.makedirs(directory)
 
+# creating queqed files and alredy crawled ones
+
+
+def dat_files(prj_nm, base_url):
+    queued = prj_nm + '/queued.txt'
+    crawled = prj_nm + '/crawled.txt'
+    if not os.path.isfile(queued):
+        write_file(queued, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+# creating a new file in python
+
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+
+def append_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+def delete_file(path):
+    with open(path, 'w'):
+        pass
